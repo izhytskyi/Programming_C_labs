205ea874c9e2e41785e2823ac8d380d6b0e3ae9f
Pavlo Izhytskyi <96439436+izhytskyi@users.noreply.github.com>
2022-02-17 13:08:33 +0200

    Update Lab0206.c
    some changes

--- Lab01/Lab0206/Lab0206.c
+++ Lab01/Lab0206/Lab0206.c
@@ -1,13 +1,7 @@
-#include <stdio.h> //це 5, треба зробити 6 :(
+#include <stdio.h>
 
 int main()
 {
-/*    int temperature_k = 0;
-    int temperature_c = 0;
-    printf("Enter temperature in Kelvin:\n");
-    scanf("%d", &temperature_k);
-    temperature_c = temperature_k + 273;
-    printf("Temperature in Celsius:%d\n(1 degree in Kelvin = %d in Celsius)", temperature_c, 1+273);*/
     
     return 0;
 }
