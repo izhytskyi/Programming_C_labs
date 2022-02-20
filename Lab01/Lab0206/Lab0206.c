09feae965d42b964024fbe8564d5dc575e5e3e76
Pavlo Izhytskyi <96439436+izhytskyi@users.noreply.github.com>
2022-02-20 20:28:21 +0200

    Update Lab0206.c

--- Lab01/Lab0206/Lab0206.c
+++ Lab01/Lab0206/Lab0206.c
@@ -2,7 +2,11 @@
 
 int main()
 {
-    
+    float galone = 16;
+    float busel = 1.2;
+    float lit = 3;
+    float res = (busel*62.0121) + ((galone/0.2642) + (lit/5.6826));
+    printf("Result: %f pintes", res);
     return 0;
 }
 
