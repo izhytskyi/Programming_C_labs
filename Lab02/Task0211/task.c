2bf04b69520284cdddbb1a00e4ddd4ba3bbcadc6
Pavlo <kn1b21.izhytskyi@kpnu.edu.ua>
2022-02-23 16:09:44 +0200

    update task 0211

--- Lab02/Task0211/task.c
+++ Lab02/Task0211/task.c
@@ -1,1 +1,20 @@
-/*11. Дано значення дійсної величини x*/
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float x, result = 0;
+    printf("Enter x:\n");
+    scanf("%f", &x);
+    float znamenyk = (pow(x,2) - 2*x - 10);
+    if(znamenyk==0){
+        printf("Division by zero!");
+    }
+    else{
+    result = 1/(pow(x,2) - 2*x - 10);
+    }
+    printf("Result = %.2f", result);
+    return 0;
+}
+
+// res = 1/(x^2 - 2*x - 10)
