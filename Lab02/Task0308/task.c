00f9f24cd44f288e8c962f6d363585f9cc676904
Pavlo <kn1b21.izhytskyi@kpnu.edu.ua>
2022-02-23 16:22:51 +0200

    update task0308

--- Lab02/Task0308/task.c
+++ Lab02/Task0308/task.c
@@ -1,4 +1,20 @@
-/*8. Визначити, чи належить точка з координатами
-x , y
-зафарбованій
-частині площини*/
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float x, y = 0;
+    printf("Enter x:\n");
+    scanf("%f", &x);
+    printf("Enter y:\n");
+    scanf("%f", &y);
+    if(((x>=-1)&&(x<=1))&&((y>=0)&&(y<=1))){
+        printf("Dot (%.2f,%.2f) in zone", x, y);
+    }
+    else{
+        printf("Dot not in zone");
+    }
+    return 0;
+}
+
+//x[-1, 1] y[0,1]
