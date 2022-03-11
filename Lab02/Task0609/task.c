5eaafacf0dbfc625348c253f69d71292bcf189fb
Pavlo <kn1b21.izhytskyi@kpnu.edu.ua>
2022-03-11 13:58:44 +0200

    update 0609

--- Lab02/Task0609/task.c
+++ Lab02/Task0609/task.c
@@ -1,2 +1,19 @@
-/*9. Дано ціле
-n . Визначити//
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double z = 0;
+    double supp = 1;
+    int n = 0;
+    int i = 0;
+    printf("Enter n:\n");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        z = z + (1 + 1/(pow(n,2)));
+        supp = supp * z;
+    }
+    printf("Supplement = %.2f", supp);
+    return 0;
+}
