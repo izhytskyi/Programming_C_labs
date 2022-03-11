68721d28da6e6020bede0cd39f414b4a298762e4
Pavlo <kn1b21.izhytskyi@kpnu.edu.ua>
2022-03-11 13:28:14 +0200

    update 0404

--- Lab02/Task0404/task.c
+++ Lab02/Task0404/task.c
@@ -1,2 +1,25 @@
-//4
-// &&
+#include <stdio.h>
+
+int main()
+{
+	float F, a, b, c, x = 0;
+	printf("Enter a:\n");
+	scanf("%f", &a);
+	printf("Enter b:\n");
+	scanf("%f", &b);
+	printf("Enter c:\n");
+	scanf("%f", &c);
+	printf("Enter x:\n");
+	scanf("%f", &x);
+	if((x<0)&&(b!=0)){
+		F = (-a)*x - c;
+	}
+	else if((x>0)&&(b==0)){
+		F = (x-a)/c;
+	}
+	else{
+		F = (b*x)/(c-a);
+	}
+	printf("%.2f", F);
+	return 0;
+}
