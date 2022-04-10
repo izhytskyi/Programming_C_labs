7978f2b8fff37e581496c66ef589acd34a92b5fc
Pavlo Izhytskyi <96439436+izhytskyi@users.noreply.github.com>
2022-04-10 14:16:49 +0300

    Update Task0113.c

--- Lab03/Task0113.c
+++ Lab03/Task0113.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
-int main(){
-return 0;
+int main()
+{
+    int a, b = 0;
+    printf("Enter a: ");
+    scanf("%d", &a);
+    printf("Enter b: ");
+    scanf("%d", &b);
+    int *pa = &a;
+    int *pb = &b;
+    *pa = *pa * 2;
+    *pb = *pb / 2;
+    printf("\na = %d\nb = %d\n", a, b);
+    return 0;
 }
