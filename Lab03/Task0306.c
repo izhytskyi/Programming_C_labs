5beafd6d6685e31c6b0604e56b6301a5e4ddf6ff
Pavlo Izhytskyi <96439436+izhytskyi@users.noreply.github.com>
2022-04-10 14:36:33 +0300

    Update Task0306.c

--- Lab03/Task0306.c
+++ Lab03/Task0306.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-return 0;
+int main()
+{
+    float *a, *b, *c = NULL;
+    a = malloc(sizeof(float));
+    b = malloc(sizeof(float));
+    c = malloc(sizeof(float));
+    printf("Enter a = ");
+    scanf("%f", a);
+    printf("Enter b = ");
+    scanf("%f", b);
+    printf("Enter c = ");
+    scanf("%f", c);
+    *a = *a / 2;
+    printf("\na = %.2f\nb = %.2f\nc = %.2f\n", *a, *b, *c);
+    return 0;
 }
