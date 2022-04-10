b33d9ece04de33c36aa07a5c99dd930b15847adb
Pavlo Izhytskyi <96439436+izhytskyi@users.noreply.github.com>
2022-04-10 14:23:09 +0300

    Update Task0213.c

--- Lab03/Task0213.c
+++ Lab03/Task0213.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 
-int main(){
-return 0;
+int main()
+{
+    float a, b, c = 0;
+    printf("Enter a(katet 1): ");
+    scanf("%f", &a);
+    printf("Enter b(katet 2): ");
+    scanf("%f", &b);
+    float *pa = &a;
+    float *pb = &b;
+    float *pc = &c;
+    *pc = sqrt(pow(*pa,2) + pow(*pb,2));
+    printf("\na = %f\nb = %f\nC = %f", a, b, c);
+    return 0;
 }
