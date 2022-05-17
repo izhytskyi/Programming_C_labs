e0402c01e07787a0a75916a17318ac24086e3c87
Pavlo Izhytskyi <96439436+izhytskyi@users.noreply.github.com>
2022-05-17 14:35:07 +0300

    Update Task0507.c

--- Lab04/Task0507.c
+++ Lab04/Task0507.c
@@ -1,1 +1,70 @@
+#include <stdio.h>
 
+int main()
+{
+    int M[100][100];
+    for(int i = 0; i<100; i++)
+    {
+        for(int j = 0; j<100; j++)
+        {
+            M[i][j] = 0;
+        }
+    }
+    int p = 0;
+    int t = 0;
+    printf("Enter P = ");
+    scanf("%d", &p);
+    printf("Enter T = ");
+    scanf("%d", &t);
+    float a_average = 0;
+    int sum = 0;
+    int elements = p*t;
+    for(int i = 0; i < p; i++)
+    {
+        for(int j = 0; j < t; j++)
+        {
+            printf("Enter M[%d][%d] = ", i, j);
+            scanf("%d", &M[i][j]);
+        }
+    }
+    for(int i = 0; i < p; i++)
+    {
+        for(int j = 0; j < t; j++)
+        {
+            sum = sum + M[i][j];
+        }
+    }
+    a_average = (float)sum/(float)elements;
+    for (int i = 0; i < p; i++)
+    {
+        for (int j = 0; j < t; j++)
+        {
+            printf("%5d ", M[i][j]);
+        }
+        printf("\n");
+    }
+    for(int i = 0; i < p; i++)
+    {
+        for(int j = 0; j < t; j++)
+        {
+            if(M[i][j] > a_average)
+            {
+                M[i][j] = 1;
+            }
+            else if(M[i][j] < a_average)
+            {
+                M[i][j] = -1;
+            }
+        }
+    }
+    printf("Average : %d/%d = %f\n", sum, elements, a_average);
+    for (int i = 0; i < p; i++)
+    {
+        for (int j = 0; j < t; j++)
+        {
+            printf("%5d ", M[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
