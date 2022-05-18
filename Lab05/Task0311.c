139b6458595d6b8b2953487691bd7bca12eb5755
Pavlo Izhytskyi <96439436+izhytskyi@users.noreply.github.com>
2022-05-18 11:51:07 +0300

    Update Task0311.c

--- Lab05/Task0311.c
+++ Lab05/Task0311.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+#include <locale.h>
+#include <malloc.h>
+int main()
+{
+    setlocale(LC_CTYPE, "ukr");
+    int mlt = 1;
+    char *str;
+    str = (char *)malloc(100);
+ 
+    printf("Введіть строку:\n");
+    fgets(str, 96, stdin);
+    for (int i = 0; str[i] != 0; i++)
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            mlt *= str[i] - '0';
+        }
+    }
+    printf("Добуток=%d\n", mlt);
+    free(str);
+    return 0;
+}
