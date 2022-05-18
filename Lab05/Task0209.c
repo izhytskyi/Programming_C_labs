58d394b86251f92cf418d34b555889d7f0f751cd
Pavlo Izhytskyi <96439436+izhytskyi@users.noreply.github.com>
2022-05-18 11:27:11 +0300

    Update Task0209.c

--- Lab05/Task0209.c
+++ Lab05/Task0209.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <locale.h>
 
+int main()
+{
+    //setlocale(LC_CTYPE, "ukr");
+    char str[100];
+    printf("Enter text:\n");
+    fgets(str, 96, stdin);
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+
+        if(str[i] == 'Z')
+        {
+          str[i] = 'A';  
+        }
+        else if(str[i] == 'z') 
+        {
+          str[i] = 'a';
+        }
+        else
+        {
+          str[i] = str[i] + 1; 
+        }
+    }
+    puts(str);
+    return 0;
+}
