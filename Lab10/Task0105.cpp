62728334d5b3bcccbee9b89fca6effcf0e68175b
Pavlo Izhytskyi <96439436+izhytskyi@users.noreply.github.com>
2022-05-24 12:48:41 +0300

    Update Task0105.cpp

--- Lab10/Task0105.cpp
+++ Lab10/Task0105.cpp
@@ -1,1 +1,31 @@
-
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main() {
+    char s[256],*words[128];
+    char *d=" ,.";
+    int count=0;
+    printf("Enter your string:\n");
+    fgets(s, 255, stdin);
+    char *p;
+    p = strtok(s, d);
+    while(p) 
+    {
+        words[count] = (char*)malloc(strlen(p)+1);
+        strcpy(words[count],p);
+        count++;
+        p = strtok(NULL, d);
+    }
+    for (int k=0; k<count/2*2; k+=2) 
+    {
+        char *temp = words[k];
+        words[k] = words[k+1];
+        words[k+1] = temp;
+    }
+    for (int i = 0; i < count; i++)
+    {
+        printf("%s ", words[i]);
+    }
+    return 0;
+}
