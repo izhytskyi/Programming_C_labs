cabd5a25171b2cc06113153a79ca8730d2889c4f
Pavlo Izhytskyi <96439436+izhytskyi@users.noreply.github.com>
2022-06-10 14:11:37 +0300

    Update Task0205.cpp

--- Lab07/Task0205.cpp
+++ Lab07/Task0205.cpp
@@ -1,4 +1,34 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void calculation(int n);
+
 int main()
 {
-return 911;
+  int n = 0;
+  cout << "Enter n(4<n<10): ";
+  cin >> n;
+  while((n < 4)||(n > 10)) 
+  {
+    cout << "4<n<10" << "\n" << "Enter n(4<n<10): ";
+    cin >> n;
+  }
+  calculation(n);
+  return 0;
+}
+
+void calculation(int n) 
+{
+  long double result;
+  double sum = 0;
+  for (int i = 1; i <= n; i++)
+  {
+    double temp;
+    cout << "a" << i << " = ";
+    cin >> temp;
+    sum = sum + pow((-1),i)*temp;
+  }
+  result = sum;
+  cout << "Result: " << result << endl;
 }
